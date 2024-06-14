67aee9ed5be4bb71bad5aa82f46e0ffa1801dfc7
Haknutiy <thehaknutiy@gmail.com>
2024-06-14 12:45:34 +0600

    feat: added checked courses

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './index.css'
 import { AppProvider } from '@/providers/app'
 import { AppRoutes } from '@/routes'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <AppProvider>
-      <AppRoutes />
+    <AppRoutes />
+    <Analytics/>
     </AppProvider>
   )
 }
